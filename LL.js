5cd8b3bb0e37c43d9e12b369e702f8a5810e3c8b
M4A1 <1285666482@qq.com>
2018-09-25 14:15:06 +0800

    modify

---
+++ LL.js
@@ -0,0 +1,29 @@
+﻿window.onload = function(argument) {
+    
+var div = document.getElementsByTagName('div')[0];
+  
+  var a = false
+  
+  div.onclick = function(argument) {
+     
+   if (a = !a) {
+    
+        div.classList.add("scla");
+    
+        document.getElementsByTagName('h1')[0].classList.add('show');
+ 
+        document.getElementsByTagName("div")[1].style.display = "none";
+ 
+       } else {
+        
+    div.classList.remove("scla");
+      
+      document.getElementsByTagName('h1')[0].classList.remove('show');
+    
+        document.getElementsByTagName("div")[1].style.display = "block"
+   
+     }
+ 
+   }
+
+}
